489cb61f7aa2a1785514ec683b3cb6249d0ed7bb
Faisal Qureshi <faisal@octaloop.io>
2026-05-15 19:28:18 +0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     server: {
-      allowedHosts: ["a6g4me6sci3s95sybrdlx5e7.coolify.octaloop.dev", "workflow-ettm.octaloop.dev"],
+      allowedHosts: [
+        "e-tolling.com",
+        "a6g4me6sci3s95sybrdlx5e7.coolify.octaloop.dev",
+        "workflow-ettm.octaloop.dev",
+      ],
     },
   },
 
