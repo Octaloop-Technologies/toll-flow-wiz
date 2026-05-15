2ca455e34c3e252247f526caf48f18657933f785
Faisal Qureshi <faisal@octaloop.io>
2026-05-15 19:12:26 +0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  vite: {
+    server: {
+      allowedHosts: ["a6g4me6sci3s95sybrdlx5e7.coolify.octaloop.dev", "workflow-ettm.octaloop.dev"],
+    },
+  },
 
   tanstackStart: {
     server: {
-      allowedHosts: ["a6g4me6sci3s95sybrdlx5e7.coolify.octaloop.dev", "workflow-ettm.octaloop.dev"],
       entry: "server",
       preset: "node", // ← Switch from cloudflare to node
     },
